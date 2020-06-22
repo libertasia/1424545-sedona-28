2d6a7bb72ab826756439cb56af3f46fe8787f8a4
Anastasiia Erokhina <erokhinaanastasiia@gmail.com>
2020-06-22 17:32:12 +0300

    Fixed review comments

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ var searchButton = document.querySelector(".search-button");
 var searchForm = document.querySelector(".search-form");
 var adultCount = searchForm.querySelector("#adults");
 var childrenCount = searchForm.querySelector("#children");
+var checkInDate = searchForm.querySelector("#check-in-date");
+var checkOutDate = searchForm.querySelector("#check-out-date");
 
 var isStorageSupported = true;
 var storage = "";
@@ -24,6 +26,7 @@ searchButton.addEventListener("click", function (evt) {
     // hide form
     searchForm.classList.remove("modal-show");
     searchForm.classList.add("modal-close");
+    searchForm.classList.remove("modal-error");
   } else {
     // show form
     if (adultCountStored) {
@@ -38,13 +41,22 @@ searchButton.addEventListener("click", function (evt) {
 });
 
 searchForm.addEventListener("submit", function (evt) {
-    if (!adultCount.value || !childrenCount.value) {
+    if (!adultCount.value || !childrenCount.value || !checkInDate.value || !checkOutDate.value) {
       evt.preventDefault();
+      searchForm.classList.remove("modal-error");
+      searchForm.offsetWidth = searchForm.offsetWidth;
+      searchForm.classList.add("modal-error");
+      if (!checkInDate.value) {
+        checkInDate.focus()
+      } else
+      if (!checkOutDate.value) {
+        checkOutDate.focus()
+      } else
+      if (!adultCount.value) {
+        adultCount.focus()
+      } else
       if (!childrenCount.value) {
         childrenCount.focus()
-      }
-      if (!adultCount.value) {
-          adultCount.focus()
       }
     } else {
         if (isStorageSupported) {
@@ -53,3 +65,14 @@ searchForm.addEventListener("submit", function (evt) {
         }
       }
   });
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (searchForm.classList.contains("modal-show")) {
+      evt.preventDefault();
+      searchForm.classList.remove("modal-show");
+      searchForm.classList.remove("modal-error");
+      searchForm.classList.add("modal-close");
+    }
+  }
+});
